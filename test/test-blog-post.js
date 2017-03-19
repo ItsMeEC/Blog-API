51d746231b5f3b81b7e38bd3304ef98379576ab9
ItsMeEC <evan.ciavaglia3@gmail.com>
2017-03-19 10:49:58 -0400

    test

--- test/test-blog-post.js
+++ test/test-blog-post.js
@@ -6,3 +6,21 @@ const should = chai.should();
 const {DATABASE_URL} = require('../config');
 const {BlogPost} = require('../models');
 const {closeServer, runServer, app} = require('../server');
+
+describe('blog posts API resource', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  beforeEach(function() {
+    return seedBlogPostData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  });
